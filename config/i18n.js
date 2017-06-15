d37ed89b068ebcb8d6ed50821ff1091a0bd7877d
claudina sarahe <cs@misscs.me>
2017-06-15 10:27:35 -0400

    Crear un archivo para routes/direcciones

--- config/i18n.js
+++ config/i18n.js
@@ -3,28 +3,12 @@ export const i18n = {
     lang: `en`,
     country: `US`,
     dir: `ltr`,
-    routes: [
-      {view: `home`, path: `/home`},
-      {view: `example`, path: `/example`},
-      {view: `donate`, path: `/donate`},
-      {view: `programs`, path: `/programs`},
-      {view: `about`, path: `/about`},
-      {view: `becas`, path: `/scholarships`},
-      {view: `news`, path: `/news`},
-    ],
+    charset: `utf-8`
   },
   es: {
     lang: `es`,
     country: `DO`,
     dir: `ltr`,
-    routes: [
-      {view: `home`, path: `/inicio`},
-      {view: `example`, path: `/ejemplo`},
-      {view: `donate`, path: `/donar`},
-      {view: `programs`, path: `/programas`},
-      {view: `about`, path: `/sobre`},
-      {view: `becas`, path: `/becas`},
-      {view: `news`, path: `/noticias`},
-    ],
+    charset: `utf-8`
   },
 };
